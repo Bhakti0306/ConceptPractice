8e14db9873bc865a258b93fee34751985d73678a
Bhakti Deshmukh <53202941+Bhakti0306@users.noreply.github.com>
2019-08-04 21:14:22 +0530

    updating garbage value

--- src/Practice1/Test1.java
+++ src/Practice1/Test1.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Test1 {
 
 	public static void main(String[] args) throws InterruptedException {
+		int i=1;
 		//opening amazon website
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\Sakuralove\\Desktop\\selenium dw\\chromedriver.exe");		
 		WebDriver driver= new ChromeDriver();
